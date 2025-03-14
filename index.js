60a0f0a8383f0e5697f65b07f48acd9529d7813c
shin <qoo080337@gmail.com>
2025-03-14 18:27:49 +0900

    test

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const URL = "https://l-tike.com/st1/l-web-gs_mlts2025/sitetop";
+const { LINE_NOTIFY_TOKEN } = process.env;
 
 async function checkTicketStatus() {
   try {
-    const response = await axios.get(URL, { timeout: 1000 });
+    const response = await axios.get(URL, {
+      timeout: 10000,
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      },
+    });
     const $ = cheerio.load(response.data);
 
     if ($("body").text().includes("発売中")) {
-      await sendTwitterNotification("チケットが発売されたかも！")
+      await sendLineNotification("チケットが発売されたかも！")
     } else {
-      await sendTwitterNotification("チケットがない！")
+      await sendLineNotification("チケットがない！")
     }
   } catch (error) {
     console.error(`エラー: ${error.message}`);
