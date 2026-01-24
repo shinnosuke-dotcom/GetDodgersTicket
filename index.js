6cc86ecd62f6012e15715d2827b9939089db7360
shin <shinkun0304@icloud.com>
2026-01-24 10:03:38 +0900

    lintの適用

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ async function checkTicketStatus() {
     const response = await axios.get(URL, {
       timeout: 10000,
       headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
       },
     });
     const $ = cheerio.load(response.data);
@@ -18,7 +19,7 @@ async function checkTicketStatus() {
     const occurrences = $("body").text().split(keyword).length - 1;
 
     if (occurrences < 20) {
-      await sendLine("イープラスの状況が変わりました！"+"\n"+URL);
+      await sendLine("イープラスの状況が変わりました！" + "\n" + URL);
       console.log("通知を送信しました。");
     }
   } catch (error) {
@@ -30,24 +31,21 @@ const ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
 const LINE_USER_ID = process.env.LINE_USER_ID;
 
 async function sendLine(message) {
-  const res = await fetch(
-    "https://api.line.me/v2/bot/message/broadcast",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-      },
-      body: JSON.stringify({
-        "messages":[
-          {
-              "type":"text",
-              "text": message
-          }
-      ]
-      }),
-    }
-  );
+  const res = await fetch("https://api.line.me/v2/bot/message/broadcast", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({
+      messages: [
+        {
+          type: "text",
+          text: message,
+        },
+      ],
+    }),
+  });
 }
 
 checkTicketStatus();
